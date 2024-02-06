ebf4d0db6133aa3d3cb75f09510dd70804dda047
Gul Noor <67622150+gulnoor@users.noreply.github.com>
2024-02-06 20:09:12 +0500

    ..

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-# from streamlit_pandas_profiling import st_profile_report
-from fastai.vision.core import PILImage
-import fastcore.all as fc
-from fastai.learner import load_learner
+from fastai.vision.widgets import *
+from fastai.vision.all import *
 import streamlit as st
 
 model = load_learner("./model.pkl")
@@ -10,6 +8,6 @@ if file:
     st.image(file)
     img  = PILImage.create((file))
     st.write(model.predict(img))
-    file=None
+
 st.checkbox('asdasd')
 print("dd")
